949179414df49f3469c4d9542385292bc219fc20
Dan Hahn <dan@svahtml.com>
2017-07-10 18:37:19 -0400

    Adding week 5 files

---
+++ week5/4-responsive/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./less/**/*.less', ['less']);
+});
+
+gulp.task('default', ['less', 'watch']);
